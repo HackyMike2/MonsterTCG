e79b80a396b22da50c6d4595af4974d7050268c3
if23b140 <if23b140@Wien-Technikum>
2024-12-02 11:47:08 +0100

    initial commit

---
+++ MonsterTCG/MonsterCard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterTCG
+{
+    public class MonsterCard : Card
+    {
+        public MonsterCard(string name, float damage, ListMaster.Type type) : base (name, damage, type) { }
+
+
+
+
+
+        public override float CalculateDamage(Card opponent)
+        {
+           // if(Name.Contains("")) //auch nicht so geiler ansatz
+           return Damage; /*Damage calkulieren*/
+        }
+    }
+
+
+}
---
+++ MonsterTCG/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterTCG
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string PasswordHash { get; set; }
+        public int Coins { get; set; } = 20;
+        //card deck, and card stack
+        public int Elo { get; set; } = 100;
+        public string SecurityToken { get; set; }
+
+
+        public void AddCoins(int amount) {Coins += amount;}
+        public void UpdateElo(int amount) 
+        {if(!(Elo + amount < 0)) {  Elo += amount; }} // can also be negative, not go under 0
+        public void GenerateToken() {/*generate token and send it to Security Token.*/ }
+
+        public void CompareToken(string token1, string token2) { }
+
+
+
+    }
+}
